805ac901a8ace33e6d3449421fbfbdfec1f89753
Achilleas Naoumidis <anaumidis@gmail.com>
2015-01-04 07:03:15 +0200

    Create error page

---
+++ public/error.php
@@ -0,0 +1,26 @@
+<?php require_once __DIR__ . '/../bootstrap.php'; ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>WeLiMe : Error</title>
+
+    <?php require_once __DIR__ . "/parts/head.php"; ?>
+
+    <link rel="Stylesheet" href="css/error.css"/>
+</head>
+
+<body>
+<?php require_once __DIR__ . "/parts/header.php"; ?>
+
+<?php require_once __DIR__ . "/parts/nav.php"; ?>
+
+<main>
+    <h3>Error</h3>
+</main>
+
+<?php require_once __DIR__ . "/parts/footer.php"; ?>
+</body>
+
+</html>
