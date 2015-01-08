54d1041e778bffe38b21a4f85f0dcaae864d38b3
Achilleas Naoumidis <anaumidis@gmail.com>
2015-01-08 22:09:03 +0200

    Refactor some code

--- public/parts/nav.php
+++ public/parts/nav.php
@@ -1,13 +1,26 @@
 <nav>
     <div id="navigation">
         <a href="index.php">Home</a>
-<?php if (isset($_SESSION['UserUsername'])) {
+<?php
+use WeLiMe\Controllers\UserController;
+use WeLiMe\Exceptions\RepositoryExceptions\UserNotFoundException;
+use WeLiMe\Repositories\UserRepository;
+
+if (isset($_SESSION['UserUsername'])) {
     echo("\t&emsp;|&emsp;<a href=\"chat.php\">Private Chat</a>\n");
 } ?>
     </div>
     <div id="actions">
 <?php if (isset($_SESSION['UserUsername'])) {
-    echo("\tHi, <a href=\"\">" . $_SESSION['UserUsername'] . "</a>!&emsp;|&emsp;\n");
+    $userRepository = new UserRepository();
+
+    try {
+        $user = $userRepository->findOneByUsername($_SESSION['UserUsername']);
+    } catch (UserNotFoundException $e) {
+        die($e->getMessage());
+    }
+
+    echo("\tLogged in as <a href=\"\">" . $user->getFirstName() . " " . $user->getLastName() . "</a>&emsp;|&emsp;\n");
     echo("\t<a href=\"logout.php\">Logout</a>\n");
  } else {
     echo("\t<a href=\"login.php\">Login</a>&emsp;|&emsp;\n");
