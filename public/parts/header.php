dbc12d6ed583f79e59bd41954dc2ffe275def715
Achilleas Naoumidis <anaumidis@gmail.com>
2015-01-08 13:44:40 +0200

    Clean up the code

--- public/parts/header.php
+++ public/parts/header.php
@@ -18,17 +18,4 @@ if (isset($_SESSION['UserUsername'])) {
 ?>
 <header>
     <div id="logo">We<span style="color: #36F;">.</span>Li<span style="color: #36F;">.</span>Me</div>
-    <?php if (isset($_SESSION['UserUsername'])) { ?>
-    <div id="createConversationForm">
-        <form name="conversationForm" id="conversationForm" action="../app/FormHandlers/ConversationFormHandler.php" method="post">
-            <table>
-                <tr>
-                    <td><label for="txtUsernames">Users to invite:</label></td>
-                    <td><input type="text" name="txtUsernames" id="txtUsernames" title="Separator(,)"/></td>
-                    <td><input type="submit" value="Invite"/></td>
-                </tr>
-            </table>
-        </form>
-    </div>
-    <?php } ?>
 </header>
